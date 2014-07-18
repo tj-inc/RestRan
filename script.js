95bdf7b1251fe91ecad3396685d3d547c89fef69
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2014-07-18 21:25:02 +0800

    [Jimmy] Baidu Map API ready, prepare to add information to the database.

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-var items = [{name:"苏氏牛肉面",logo:""},
-			{name:"吉野家",logo:""},
-			{name:"DQ",logo:""},
-		    {name:"红薯地瓜坊",logo:""},
-		    {name:"周黑鸭",logo:""},
-		    {name:"饺子馆",logo:""},
-		    {name:"驴火",logo:""},
-		    {name:"水果干果店",logo:""},
-		    {name:"711",logo:""},
-		    {name:"桂林粉",logo:""},
-		    {name:"肯德基",logo:""},
-		    {name:"韩国烤肉",logo:""},
-		    {name:"香草香草",logo:""},
-		    {name:"东来顺",logo:""},
-		    {name:"杭州小笼包",logo:""},
-		    {name:"kro's nest pizza",logo:""},
-		    {name:"coco茶饮",logo:""},
-		    {name:"陕西风味",logo:""},
-		    {name:"兰州拉面",logo:""},
-		    {name:"万福烤鸭",logo:""},
-		    {name:"巫山烤全鱼",logo:""},
-		    {name:"永和大王",logo:""},
-		    {name:"香猪坊韩国料理",logo:""},
-		    {name:"面香",logo:""},
-		    {name:"金凤成翔",logo:""},
-		    {name:"大粥锅",logo:""},
-		    {name:"重庆麻辣烫",logo:""},
-		    {name:"砂锅米线",logo:""},
-		    {name:"羊蝎子大棒骨",logo:""},
-		    {name:"传奇酸辣粉",logo:""},
-		    {name:"沙县小吃",logo:""},
-		    {name:"麦多馅饼",logo:""},
-	     	{name:"苏皇肉夹馍酸奶",logo:""},
-	     	{name:"合利屋",logo:""},
-		    {name:"黄焖鸡米饭",logo:""},
-		    {name:"百年卤煮",logo:""},
-		    {name:"食立方",logo:""},
-		    {name:"山西刀削面",logo:""},
-		    {name:"霄云羊杂割",logo:""}];
+var items = [{name:"苏氏牛肉面",logo:"",lat:"",lng:"",addr:""},
+			{name:"吉野家",logo:"",lat:"",lng:"",addr:""},
+			{name:"DQ",logo:"",lat:"",lng:"",addr:""},
+		    {name:"红薯地瓜坊",logo:"",lat:"",lng:"",addr:""},
+		    {name:"周黑鸭",logo:"",lat:"",lng:"",addr:""},
+		    {name:"饺子馆",logo:"",lat:"",lng:"",addr:""},
+		    {name:"驴火",logo:"",lat:"",lng:"",addr:""},
+		    {name:"水果干果店",logo:"",lat:"",lng:"",addr:""},
+		    {name:"711",logo:"",lat:"",lng:"",addr:""},
+		    {name:"桂林粉",logo:"",lat:"",lng:"",addr:""},
+		    {name:"肯德基",logo:"",lat:"",lng:"",addr:""},
+		    {name:"韩国烤肉",logo:"",lat:"",lng:"",addr:""},
+		    {name:"香草香草",logo:"",lat:"",lng:"",addr:""},
+		    {name:"东来顺",logo:"",lat:"",lng:"",addr:""},
+		    {name:"杭州小笼包",logo:"",lat:"",lng:"",addr:""},
+		    {name:"Kro's Nest Pizza",logo:"",lat:"",lng:"",addr:""},
+		    {name:"Coco茶饮",logo:"",lat:"",lng:"",addr:""},
+		    {name:"陕西风味",logo:"",lat:"",lng:"",addr:""},
+		    {name:"兰州拉面",logo:"",lat:"",lng:"",addr:""},
+		    {name:"万福烤鸭",logo:"",lat:"",lng:"",addr:""},
+		    {name:"巫山烤全鱼",logo:"",lat:"",lng:"",addr:""},
+		    {name:"永和大王",logo:"",lat:"",lng:"",addr:""},
+		    {name:"香猪坊韩国料理",logo:"",lat:"",lng:"",addr:""},
+		    {name:"面香",logo:"",lat:"",lng:"",addr:""},
+		    {name:"金凤成翔",logo:"",lat:"",lng:"",addr:""},
+		    {name:"大粥锅",logo:"",lat:"",lng:"",addr:""},
+		    {name:"重庆麻辣烫",logo:"",lat:"",lng:"",addr:""},
+		    {name:"砂锅米线",logo:"",lat:"",lng:"",addr:""},
+		    {name:"羊蝎子大棒骨",logo:"",lat:"",lng:"",addr:""},
+		    {name:"传奇酸辣粉",logo:"",lat:"",lng:"",addr:""},
+		    {name:"沙县小吃",logo:"",lat:"",lng:"",addr:""},
+		    {name:"麦多馅饼",logo:"",lat:"",lng:"",addr:""},
+	     	{name:"苏皇肉夹馍酸奶",logo:"",lat:"",lng:"",addr:""},
+	     	{name:"合利屋",logo:"",lat:"",lng:"",addr:""},
+		    {name:"黄焖鸡米饭",logo:"",lat:"",lng:"",addr:""},
+		    {name:"百年卤煮",logo:"",lat:"",lng:"",addr:""},
+		    {name:"食立方",logo:"",lat:"",lng:"",addr:""},
+		    {name:"山西刀削面",logo:"",lat:"",lng:"",addr:""},
+		    {name:"霄云羊杂割",logo:"",lat:"",lng:"",addr:""}];
 	
 var index = 0;
 var timer = 0;
@@ -73,17 +73,22 @@ function setTimer() {
 		timer = setTimeout('setTimer()', 50);
 }
 
-function moreInfo(num) {
-		document.getElementById("myModalLabel").innerHTML = document.getElementById(""+num).innerHTML;
-		if (index==0)
-		{
-			document.getElementById("myModalBody").innerHTML = items[num].name;
-
-		}
-		else
-		{
-			theItemIndex = (index + 1) % 39 /* Why plus 1, not 2??? */
-			document.getElementById("myModalBody").innerHTML = items[theItemIndex].name;
-		}
-
-}
+//Baidu Map API
+function gotobaidu(type)
+{
+    if($.trim($("input[name=origin]").val())=="")
+    {
+        alert("请输入起点！");
+        return;
+    }else{
+        if(type==1)
+        {
+            $("input[name=mode]").val("transit");
+            $("#gotobaiduform")[0].submit();
+        }else if(type==2)
+        {    
+            $("input[name=mode]").val("driving");        
+            $("#gotobaiduform")[0].submit();
+        }
+    }
+}   
