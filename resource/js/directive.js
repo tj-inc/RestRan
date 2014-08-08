3808a76649a9d8e079a54e43b7904de6fc92de69
JiaHang Li <jiahang_li@outlook.com>
2014-08-08 09:32:28 +0800

    [Tak] Change file arrangements and corresponding linkings, bug: unknown access to favicon.ico

---
+++ resource/js/directive.js
@@ -0,0 +1,27 @@
+//Logo part of webpage
+restran.directive("logo", function(){
+    return {restrict: "E", templateUrl: "./resource/partials/logo.html"};
+});
+
+//Prompt part of webpage
+restran.directive("promptLine", function(){
+    return {restrict: "E", templateUrl: "./resource/partials/prompt-line.html"};
+});
+
+//Main part of webpage
+restran.directive("main", function(){
+    return {restrict: "E",
+        templateUrl: "./resource/partials/main.html",
+        controller: mainControl,
+        controllerAs: "mCtrl"};
+});
+
+//Footing part of webpage
+restran.directive("footing", function(){
+    return {restrict: "E", templateUrl: "./resource/partials/footing.html"};
+});
+
+//Hidden Modal of webpage
+restran.directive("modal", function(){
+    return {restrict: "E", templateUrl: "./resource/partials/modal.html"};
+});
