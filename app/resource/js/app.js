7c8f70ca4b650e1437612ca4233ca78bdf6e80a5
JiaHang Li <jiahang_li@outlook.com>
2014-08-12 01:10:15 +0800

    [Tak] Use MongoDB to makke access to the JSON object with restaurant information successfully and updated server.js

--- app/resource/js/app.js
+++ app/resource/js/app.js
@@ -1,6 +1,6 @@
 //Fetch the JSON data of restaurants
 var dataArray;
-$.getJSON("./rest-list.json", function(data){dataArray = data;});
+$.getJSON("rest-list.json", function(data){dataArray = data;});
 
 //Define the Angular Module
 var restran = angular.module("restran", []);
--- server.js
+++ server.js
@@ -1,8 +1,46 @@
-var http = require('http');
-var express = require('express');
+var http = require("http");
+var fs = require("fs");
+var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 
-app.use(express.static(__dirname + '/app'));
+app.use(function(req, res, next){
+    console.log("%s %s", req.method, req.url);
+    next();
+});
+
+app.use(express.static(__dirname + "/app"));
+
+app.use(function(req, res, next){
+    if(req.url == "/rest-list.json") {
+        mongoose.connect("mongodb://localhost/test");
+        var db = mongoose.connection;
+        db.on("error", console.error.bind(console, "DataBase Connection Error: "));
+        db.once("open", function(){
+            console.log("DataBase Connection Success!");
+            var restSchema = mongoose.Schema({
+                "chinese": Boolean,
+                "name": String,
+                "lat": String,
+                "lng": String,
+                "addr": String,
+                "tel": String
+            });
+
+            restSchema.set("toJSON");
+
+            var restModel = mongoose.model("restrans", restSchema);
+
+            restModel.find(function(err, rests){
+                if (err) console.error(err);
+                console.log("DataBase Querying Success!");
+                res.send(rests);
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 app.listen(3000);
 
